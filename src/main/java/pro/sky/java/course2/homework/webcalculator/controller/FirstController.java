604f444ec0c4faae41d1be639bcf337eee4bbe36
konstantin <mom.konstantin@gmail.com>
2023-03-25 15:08:34 +0300

    Сделано ДЗ

--- src/main/java/pro/sky/java/course2/homework/webcalculator/controller/FirstController.java
+++ src/main/java/pro/sky/java/course2/homework/webcalculator/controller/FirstController.java
@@ -1,7 +1,40 @@
 package pro.sky.java.course2.homework.webcalculator.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.java.course2.homework.webcalculator.service.CalculatorService;
 
 @RestController
+@RequestMapping("/calculator")
 public class FirstController {
+    private final CalculatorService calculatorService;
+
+    public FirstController( CalculatorService calculatorService ) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String greeting() { return calculatorService.greetings(); }
+
+    @GetMapping(path = "/plus")
+    public String additionNum(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return calculatorService.additionNum(num1, num2);
+    }
+
+    @GetMapping(path = "/minus")
+    public String subtractionNum(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return calculatorService.subtractionNum(num1, num2);
+    }
+
+    @GetMapping(path = "/multiply")
+    public String multiplicationNum(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return calculatorService.multiplicationNum(num1, num2);
+    }
+
+    @GetMapping(path = "/divide")
+    public String divisionNum (@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return calculatorService.divisionNum(num1, num2);
+    }
 }
---
+++ src/main/java/pro/sky/java/course2/homework/webcalculator/service/CalculatorService.java
@@ -0,0 +1,10 @@
+package pro.sky.java.course2.homework.webcalculator.service;
+
+public interface CalculatorService {
+    String greetings();
+    String additionNum(int num1, int num2);
+    String subtractionNum(int num1, int num2);
+    String multiplicationNum(int num1, int num2);
+    String divisionNum(int num1, int num2);
+
+}
